41e93866e92da1fb5a9878f45f5b2419b1d6f958
agluzhin <agluzhin@gmail.com>
2025-11-24 16:46:06 +0300

    Initial commit: base 'Spring Boot application' project structure

---
+++ src/main/java/agluzhin/personal_finance_system/PersonalFinanceSystemApplication.java
@@ -0,0 +1,13 @@
+package agluzhin.personal_finance_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agluzhin/personal_finance_system/PersonalFinanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package agluzhin.personal_finance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
